f4951a9d42fefdd837d650cd9c58d62f8c83c15c
Lautarosco <69166345+Lautarosco@users.noreply.github.com>
2025-07-13 12:38:20 -0300

    Actualizar límites de integral y salida PID en la configuración del dron.FirstOrderIIR es una funcion visible de drone.h y ahora toma directamente alpha. Incluir ganancias de KB en vTaskPrint.

--- Flight-Controller/components/state_machine/src/StControl.c
+++ Flight-Controller/components/state_machine/src/StControl.c
@@ -70,7 +70,7 @@ void StControlFunc( drone_t * drone ) {
 
     /* Update MMA inputs with PID outputs */
     drone->attributes.components.mma.input[ C_ROLL ] = CRolld;
-    drone->attributes.components.mma.input[ C_PITCH ] = CPitchd;
+    drone->attributes.components.mma.input[ C_PITCH ] = CPitchd*0; // Ignore pitch control for now
     drone->attributes.components.mma.input[ C_Z ] = CZd; // Z control is not implemented yet
 
     float range = drone->attributes.components.pwm[ 0 ].dc_max - drone->attributes.components.pwm[ 0 ].dc_min;
