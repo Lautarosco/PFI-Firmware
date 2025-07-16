66d9c088d68a5266b4dd22538146d182c7e06b61
Juanigrandinetti <juani.grandinetti2001@gmail.com>
2025-07-16 00:08:43 -0300

    [PUSHEO AL REMOTO ANTES DE VIAJAR A ESPAÑA] Implementación del driver para el sensor LSM6DSO de STM. Como detalle final, me queda jugar con las frecuencias de corte de los filtros para el acelerómetro y el giróscopo, y ver como cambian las mediciones del sensor.

---
+++ Side-Projects/LSM6DSO/components/lsm6dso/include/hardware_layer/modes/accelerometer/lsm6dso_acc_modes.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#define LSM6DSO_ACC_SO_2G             0.000061f                 /* g/LSB */
+#define LSM6DSO_ACC_SO_4G             0.000122f                 /* g/LSB */
+#define LSM6DSO_ACC_SO_8G             0.000244f                 /* g/LSB */
+#define LSM6DSO_ACC_SO_16G            0.000488f                 /* g/LSB */
+
+typedef enum lsm6dso_odr_acc {
+    LSM6DSO_ACC_ODR_POWER_DOWN,
+    LSM6DSO_ACC_ODR_12P5_HZ,
+    LSM6DSO_ACC_ODR_26_HZ,
+    LSM6DSO_ACC_ODR_52_HZ,
+    LSM6DSO_ACC_ODR_104_HZ,
+    LSM6DSO_ACC_ODR_208_HZ,
+    LSM6DSO_ACC_ODR_416_HZ,
+    LSM6DSO_ACC_ODR_833_HZ,
+    LSM6DSO_ACC_ODR_1666_HZ,
+    LSM6DSO_ACC_ODR_3330_HZ,
+    LSM6DSO_ACC_ODR_6660_HZ,
+    LSM6DSO_ACC_ODR_1P6_HZ_HM_MODE_1
+} lsm6dso_odr_acc_t;
+
+/* The order of frequency elements MUST coincide with lsm6dso_odr_acc_t enumeration */
+extern float acc_freq_hz[12];
+
+typedef enum lsm6dso_fs_acc {
+    LSM6DSO_ACC_FS_2G,
+    LSM6DSO_ACC_FS_16G,
+    LSM6DSO_ACC_FS_4G,
+    LSM6DSO_ACC_FS_8G
+} lsm6dso_fs_acc_t;
+
+typedef enum lsm6dso_lpf2_en_acc {
+    LSM6DSO_ACC_LPF2_DISABLE,
+    LSM6DSO_ACC_LPF2_ENABLE
+} lsm6dso_lpf2_en_acc_t;
+
+typedef struct lsm6dso_acc {
+    float x;                        /* x-axis [m/s^2] */
+    float y;                        /* y-axis [m/s^2] */
+    float z;                        /* z-axis [m/s^2] */
+} lsm6dso_acc_t;
+
+typedef enum lsm6dso_acc_unit {
+    LSM6DSO_ACC_UNIT_G,                 /* Accelerometer unit is g */
+    LSM6DSO_ACC_UNIT_MS2                /* Accelerometer unit is m/s^2 */
+} lsm6dso_acc_unit_t;
